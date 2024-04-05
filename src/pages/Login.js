084d546ac0fb62a33494a1272f745d78365d28a5
Thales Lima <thales@inovaebiz.com.br>
2024-04-05 16:36:28 -0300

    Initial commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,68 +1,120 @@
 import React, { useState } from 'react';
 import { AiFillLinkedin } from 'react-icons/ai';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion, useAnimation } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 export default function Login() {
 
-  const [login, setLogin] = useState(true);
-  const [sign, setSign] = useState(false);
+  const [slide, setSlide] = useState(true);
+  const infoControl = useAnimation();
+  const formControl = useAnimation();
 
-  const toogleLogin = () => {
-    if (login === true) {
-      setLogin(false);
-      setSign(true);
+  const welcomeBack = useAnimation();
+  const welcome = useAnimation();
+  const loginAction = useAnimation();
+  const createAction = useAnimation();
+
+  const toogle = () => {
+    setSlide(!slide);
+
+    if (slide) {
+      welcome.start({ x: 650, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+      createAction.start({ x: -420, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+      welcomeBack.start({ x: -650, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+      loginAction.start({ x: 420, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+
+      welcome.start({ opacity: 1, display: 'block' }, { duration: 1 }); // Show
+      welcome.start({ x: 0 }, { duration: 1 }); // Show
+      createAction.start({ opacity: 1, display: 'block' }, { duration: 1 }); // Show
+      createAction.start({ x: 0 }, { duration: 1 }); // Show
+
+      infoControl.start({ x: '60.3vh' }, { duration: 1 }); // Slide
+      formControl.start({ x: '-40vh' }, { duration: 1 }); // Slide
     } else {
-      setLogin(true);
-      setSign(false);
+      welcomeBack.start({ x: -650, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+      loginAction.start({ x: 420, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+      welcome.start({ x: 650, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+      createAction.start({ x: -420, opacity: 0, display: 'none' }, { duration: 0 }); // Hide
+
+      welcomeBack.start({ opacity: 1, display: 'block' }, { duration: 1 }); // Show
+      welcomeBack.start({ x: 0 }, { duration: 1 }); // Show
+      loginAction.start({ opacity: 1, display: 'block' }, { duration: 1 }); // Show
+      loginAction.start({ x: 0 }, { duration: 1 }); // Show
+
+      infoControl.start({ x: '0%' }, { duration: 1 }); // Slide
+      formControl.start({ x: '0%' }, { duration: 1 }); // Slide
     }
   };
 
-  const toogleSign = () => {
-    if (sign === true) {
-      setSign(false);
-      setLogin(true);
-    } else {
-      setSign(true);
-      setLogin(false);
-    }
-  };
+  const [width, setWidth] = useState(window.innerWidth);
+
+  setTimeout(() => {
+    // alert('Width: ' + width)
+  }, 0);
 
   return (
     <div className='container2 center'>
-      <div className="loginForm">
 
-        {login === true && (
+      {width === 2134 && (
+        <div className='login-form'>
           <AnimatePresence>
-            <motion.div className="width100"
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              transition={{ duration: 0.5 }}
-              exit={{ scale: 0 }} >
-              <span onClick={() => toogleLogin()}>login</span><br/><br/>
-              <Link to="/app"><button>Login</button></Link>
+            <motion.div className='info' animate={infoControl}>
+
+              <AnimatePresence>
+                <motion.div initial={{ x: 0, opacity: 1, display: 'block' }} animate={welcomeBack}>
+                  <span className='info-txt' onClick={() => toogle()}>Welcome back!</span>
+                </motion.div>
+              </AnimatePresence>
+
+
+              <AnimatePresence>
+                <motion.div initial={{ x: 650, opacity: 0, display: 'none' }} animate={welcome}>
+                  <span className='info-txt' onClick={() => toogle()}>Welcome!</span>
+                </motion.div>
+              </AnimatePresence>
+
             </motion.div>
           </AnimatePresence>
-        )}
 
-        {sign === true && (
           <AnimatePresence>
-            <motion.div className="width100"
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              transition={{ duration: 0.5 }}
-              exit={{ scale: 0 }} >
-              <span onClick={() => toogleSign()}>create</span>
+            <motion.div className='login' animate={formControl}>
+
+              <AnimatePresence>
+                <motion.div initial={{ x: 0, opacity: 1, display: 'block' }} animate={loginAction}>
+                  <span className='login-txt' onClick={() => toogle()}>Login</span>
+                  <br /><br /><Link to='/app'><button>Acessar</button></Link>
+                </motion.div>
+              </AnimatePresence>
+
+              <AnimatePresence>
+                <motion.div initial={{ x: -420, opacity: 0, display: 'none' }} animate={createAction}>
+                  <span className='login-txt' onClick={() => toogle()}>Create Account</span>
+                </motion.div>
+              </AnimatePresence>
+
             </motion.div>
           </AnimatePresence>
-        )}
+        </div>
+      )}
 
+      {width < 2134 && width >= 1000 && (
+        <div className='login-form2'>
+
+        </div>
+      )}
+
+      {width <= 1000 && (
+        <div className='login-form3'>
+
+        </div>
+      )}
+
+      <div className='rights-container'>
+        <a href='https://www.linkedin.com/in/thaleslj/' className='no-decoration'>
+          <span className='rights'>by Thales Lima </span>
+          <AiFillLinkedin className='rights-linkedin' />
+        </a>
       </div>
-
-      <a href='https://www.linkedin.com/in/thaleslj/' className='rights-container'>
-        <span className='rights'>by Thales Lima </span>
-        <AiFillLinkedin className='rights-linkedin' />
-      </a>
     </div>
   );
 }
