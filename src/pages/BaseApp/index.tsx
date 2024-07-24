c1e0f9a983430db521c5c24fcb8db3cbd4671962
Thales Lima <thales@inovaebiz.com.br>
2024-07-23 23:29:33 -0300

    Ajustes visuais na navbar

--- src/pages/BaseApp/index.tsx
+++ src/pages/BaseApp/index.tsx
@@ -4,14 +4,14 @@ import "./styles.css";
 //import { AnimatePresence, motion } from 'framer-motion';
 import { Link, Outlet } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
-import logo from '../../assets/icone-positivo/mipmap-xxxhdpi/icon.png';
+import logo from '../../assets/O-positivo-simples.png';
 
 export default function BaseApp() {
   return (
     <>
-      <Navbar variant="dark" expand="lg" fixed="top" style={{ backgroundColor: '#a87861' }}>
+      <Navbar className="custom-navbar" variant="dark" expand="lg" fixed="top">
         <Navbar.Brand as={Link} to="/">
-          <img src={logo} alt="Logo" style={{ height: '50px', marginLeft: '20px' }} />
+          <img className='navbar-logo' src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
